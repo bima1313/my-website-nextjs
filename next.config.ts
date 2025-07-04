ab934e0cbf0d2cedc654fc3d01b7df00829e6f0a
bima1313 <bimairfy13@gmail.com>
2025-07-04 20:48:43 +0700

    optimized next config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL(`${process.env.NEXT_PUBLIC_PROFILE_URL}`),
-      new URL(`${process.env.NEXT_PUBLIC_PORTFOLIO_1}`),
-      new URL(`${process.env.NEXT_PUBLIC_PORTFOLIO_2}`),
-      new URL(`${process.env.NEXT_PUBLIC_PORTFOLIO_3}`),
-      new URL(`${process.env.NEXT_PUBLIC_PORTFOLIO_4}`),
-      new URL(`${process.env.NEXT_PUBLIC_PORTFOLIO_5}`),
+    {
+      protocol:'https',
+      hostname: `${process.env.SUPABASE_HOSTNAME}`,
+      pathname: '/**'
+    },
     ],
   },
 };
